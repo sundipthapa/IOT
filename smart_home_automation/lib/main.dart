decf576b339f449a74bf41e17c2c0c627ba907b9
sunsilthapa <sunsil1281@gmail.com>
2023-06-06 09:08:13 +0545

    main.dart updated

--- smart_home_automation/lib/main.dart
+++ smart_home_automation/lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+// import 'FanControl.dart';
+// import 'LightControl.dart';
+// import 'SensorControl.dart';
+// import 'apptheme.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'HOME AUTOMATION'),
+    );
+  }
+}
