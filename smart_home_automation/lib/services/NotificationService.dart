7278e147a58e5ef3485a505134b1fe9e53f2edb3
sundipthapa <lakhanmgr123@gmail.com>
2023-06-11 13:06:44 +0545

    display notify

--- smart_home_automation/lib/services/NotificationService.dart
+++ smart_home_automation/lib/services/NotificationService.dart
@@ -9,19 +9,20 @@ import 'package:http/http.dart' as http;
 
 class NotificationService {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
-  FlutterLocalNotificationsPlugin();
+      FlutterLocalNotificationsPlugin();
 
   static void initialize() {
     final InitializationSettings initializationSettings =
-    InitializationSettings(
-        android: AndroidInitializationSettings("@mipmap/ic_launcher"),
-        iOS: DarwinInitializationSettings(
-            requestSoundPermission: false,
-            requestBadgePermission: false,
-            requestAlertPermission: false));
+        InitializationSettings(
+            android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+            iOS: DarwinInitializationSettings(
+                requestSoundPermission: false,
+                requestBadgePermission: false,
+                requestAlertPermission: false));
     _notificationsPlugin.initialize(initializationSettings,
         onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
   }
+
   static BuildContext? context;
 
   static void onDidReceiveNotificationResponse(NotificationResponse response) {
@@ -30,15 +31,16 @@ class NotificationService {
       print(response.payload);
     }
   }
+
   static Future<void> display(
       String title, String body, String payload, BuildContext _context) async {
     context = _context;
 
     // if image from asset
     final bigpicture =
-    await getImageFilePathFromAssets('images/logo.png', 'bigpicture');
+        await getImageFilePathFromAssets('images/logo.png', 'bigpicture');
     final smallpicture =
-    await getImageFilePathFromAssets('images/logo.png', 'smallpicture');
+        await getImageFilePathFromAssets('images/logo.png', 'smallpicture');
 
     final styleinformationDesign = BigPictureStyleInformation(
       FilePathAndroidBitmap(smallpicture),
@@ -76,3 +78,40 @@ class NotificationService {
     await file.writeAsBytes(response.bodyBytes);
     return filePath;
   }
+
+  static void displayFcm(
+      {required RemoteNotification notification,
+      BuildContext? buildContext,
+      String? payload}) async {
+    try {
+      if (buildContext != null) {
+        context = buildContext;
+      }
+      var styleinformationDesign;
+      if (notification.android!.imageUrl != null) {
+        final bigpicture = await _downloadAndSaveFile(
+            notification.android!.imageUrl.toString(), 'bigPicture');
+        final smallpicture = await _downloadAndSaveFile(
+            notification.android!.imageUrl.toString(), 'smallIcon');
+        styleinformationDesign = BigPictureStyleInformation(
+          FilePathAndroidBitmap(smallpicture),
+          largeIcon: FilePathAndroidBitmap(bigpicture),
+        );
+      }
+
+      final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+      final NotificationDetails notificationDetails = NotificationDetails(
+          android: AndroidNotificationDetails("myapp", "myapp channel",
+              channelDescription: "myapp channel description",
+              importance: Importance.max,
+              priority: Priority.high,
+              styleInformation: styleinformationDesign),
+          iOS: DarwinNotificationDetails());
+      await _notificationsPlugin.show(
+          id, notification.title, notification.body, notificationDetails,
+          payload: payload);
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+  }
+}
