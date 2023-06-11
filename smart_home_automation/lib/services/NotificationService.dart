ccb68f2c74f6e50327606872eb30d75528dfeb64
sundipthapa <lakhanmgr123@gmail.com>
2023-06-11 13:05:58 +0545

    file updated

--- smart_home_automation/lib/services/NotificationService.dart
+++ smart_home_automation/lib/services/NotificationService.dart
@@ -66,3 +66,13 @@ class NotificationService {
 
     return file.path;
   }
+
+  static Future<String> _downloadAndSaveFile(
+      String url, String fileName) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final String filePath = '${directory.path}/$fileName';
+    final http.Response response = await http.get(Uri.parse(url));
+    final File file = File(filePath);
+    await file.writeAsBytes(response.bodyBytes);
+    return filePath;
+  }
