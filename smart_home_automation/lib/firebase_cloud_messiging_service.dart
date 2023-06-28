e9cf2665e714790add7bed8d9672d1c4e8a3b84a
sunsilthapa <sunsil1281@gmail.com>
2023-06-28 13:37:50 +0545

    fcm started

---
+++ smart_home_automation/lib/firebase_cloud_messiging_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class FCMService {
+  static const String FCMAPI =
+      "AAAAdEtj-Ew:APA91bEIGzKOCAmCnnAVq8uXry_qtZKteFJGX08i_2X5zYYrJ-OmVS_iAb4Evs9XNeSDAfaP9JIsr-05hApJ_EQKKKhYzuTa2UwAKRA-Fdq16VNhLTE-OO5WxDin8-V6iSx-Q5PzihTA";
+  static String makePayLoadWithToken(String? token, Map<String, dynamic> data,
+      Map<String, dynamic> notification) {
+    return jsonEncode({
+      'to': token,
+      'data': data,
+      'notification': notification,
+    });
+  }
+
+
+}
