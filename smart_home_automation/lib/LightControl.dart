b62ffad3c750c500d931648fde131d2877ad788b
sunsilthapa <sunsil1281@gmail.com>
2023-07-07 11:58:21 +0545

    file modified

---
+++ smart_home_automation/lib/LightControl.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:home_automation_system/main.dart';
+import 'apptheme.dart';
+
+import 'package:flutter/material.dart';
+import 'package:home_automation_system/main.dart';
+
+import 'apptheme.dart';
+
+class LightControlPage extends StatefulWidget {
+  @override
+  State<LightControlPage> createState() => _LightControlPageState();
+}
