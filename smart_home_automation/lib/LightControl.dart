ab985e19e537e9922fc697c57597440209378b2d
sunsilthapa <sunsil1281@gmail.com>
2023-07-07 11:59:43 +0545

    file added

--- smart_home_automation/lib/LightControl.dart
+++ smart_home_automation/lib/LightControl.dart
@@ -12,3 +12,28 @@ class LightControlPage extends StatefulWidget {
   @override
   State<LightControlPage> createState() => _LightControlPageState();
 }
+
+class _LightControlPageState extends State<LightControlPage> {
+  bool kitchenLightOn = false;
+  bool bedroomLightOn = false;
+  bool meetingLightOn = false;
+  String kitchenLight = 'OFF';
+  String bedroomLight = 'OFF';
+  String meetingLight = 'OFF';
+
+  void sendKitchenLightStatusToFirebase(String status) {
+    DatabaseReference databaseReference = FirebaseDatabase.instance
+        .reference()
+        .child('LightControl')
+        .child('KitchenLightStatus');
+    databaseReference.set(status);
+  }
+
+  void sendMeetingLightSatusToFirebase(String status) {
+    DatabaseReference databaseReference = FirebaseDatabase.instance
+        .reference()
+        .child('LightControl')
+        .child('MeetingLightStatus');
+    databaseReference.set(status);
+  }
+}
